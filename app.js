b6f1a321d786bba77075e6490017703d0c8e2f9f
neelein <shakya12210305@gmail.com>
2022-07-29 11:36:30 +0800

    2022-7-29 Gym-single-page-website

---
+++ app.js
@@ -0,0 +1,68 @@
+const menu=document.querySelector('.menu')
+const toggleMenu=document.querySelector('.toggle-menu')
+const menuLink=document.querySelectorAll('.menu-item a')
+
+toggleMenu.addEventListener('click',()=>{
+    toggleMenu.classList.toggle('active')
+    menu.classList.toggle('active')
+})
+menuLink.forEach(link=>{
+    link.addEventListener('click',()=>{
+        menu.classList.remove('active')
+        toggleMenu.classList.remove('active')
+    })
+})
+
+//animation
+
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from('.animate-home',{
+    duration:0.6,
+    opacity:0,
+    x:-200,
+    stagger:0.3
+})
+
+gsap.from('.animate-serve',{
+    scrollTrigger:'.animate-serve',
+    duration:1.0,
+    opacity:0,
+    x:-200,
+    stagger:0.1,
+    delay:0.2
+})
+
+gsap.from('.animate-img',{
+    scrollTrigger:'.animate-img',
+    duration:1.0,
+    opacity:0,
+    x:200,
+    stagger:0.1,
+    delay:0.2
+})
+
+gsap.from('.animate-team',{
+    scrollTrigger:'.animate-team',
+    duration:0.6,
+    opacity:0,
+    y:-100,
+    stagger:0.4,
+    delay:0.3
+})
+gsap.from('.animate-team-card',{
+    scrollTrigger:'.animate-team-card',
+    duration:0.6,
+    opacity:0,
+    y:-100,
+    stagger:0.4,
+    delay:0.5
+})
+
+gsap.from('.animate-email',{
+    scrollTrigger:'.animate-email',
+    duration:1.0,
+    opacity:0,
+    x:-200,
+    stagger:0.1
+})
